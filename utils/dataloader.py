f214e73a9fa2b26bb3437e9e500392b2a1487f43
Fatemeh Ch <fatemechaji76@gmail.com>
2023-11-16 19:08:46 -0500

    modified transforms functions

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -3,33 +3,29 @@ from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, random_split
 import torch
 
-def get_dataloader(root, batch_size=32, train=True):
-    # # Define the padding size
-    # padding_size = 10
-    #
-    # transform = transforms.Compose([
-    #     transforms.Resize((64, 64)),
-    #     transforms.Pad(padding_size),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    # ])
-
+def git git git (root, batch_size=32, train=True):
     # Imagenet standards
     mean = [0.485, 0.456, 0.406]
     std = [0.229, 0.224, 0.225]
-    padding_size = 10
 
     # Train uses data augmentation
-    transform = transforms.Compose([
-        transforms.Resize((64, 64)),
-        transforms.Pad(padding_size),
+    train_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
         transforms.RandomRotation(10),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
     ])
 
-    dataset = ImageFolder(root=root, transform=transform)
+    # Validation does not use augmentation
+    test_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(torch.Tensor(mean), torch.Tensor(std))
+    ])
+
+    # Apply transforms to the datasets directly
+    dataset = ImageFolder(root=root, transform=train_transforms if train else test_transforms)
 
     # Split dataset into train and validation sets
     train_size = int(0.8 * len(dataset))
